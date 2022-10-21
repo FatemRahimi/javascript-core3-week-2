7a8893ba85c72cc5aae2659b1be2fe4f3bd08118
Fatemeh Rahimi <fa.rahimi5475@gmail.com>
2022-10-21 14:32:52 +0100

    new update dog-photo-gallery

--- challenges/dog-photo-gallery/script.js
+++ challenges/dog-photo-gallery/script.js
@@ -1,16 +1,22 @@
-let button = document.getElementById("addImage");
-let ulTag = document.getElementsByTagName("ul");
-let liTag = document.getElementById("list");
-let imgTag = document.createElement("img");
 
-button.addEventListener("click", addImage());
-
-function addImage() {
+const button = document.querySelector(".button");
+const ul = document.querySelector(".ul");
+const getImage = () => {
   fetch("https://dog.ceo/api/breeds/image/random")
-    .then((response) => response.json())
+    .then((res) => res.json())
     .then((data) => {
-      imgTag.src = data.message;
+      const liNew = document.createElement("li");
+      liNew.innerHTML = `<div id='container'>
+            <div class='container'>
+            <div class='img_div'><img src='${data.img}'/><div>
+            
+            </div>
+            </div>`;
+      ul.appendChild(liNew);
     })
-    .catch((err) => console.log(err));
-}
-liTag.append(imgTag);
+        .catch((err) => console.log(err));
+
+button.addEventListener("click", getImage);
+
+/*
+const button=document.queryElement
