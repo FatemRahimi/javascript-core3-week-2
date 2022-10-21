b0966cb8d0fac23beaf926add67b04b376a4275a
Fatemeh Rahimi <fa.rahimi5475@gmail.com>
2022-10-21 22:00:35 +0100

    update

--- challenges/dog-photo-gallery/script.js
+++ challenges/dog-photo-gallery/script.js
@@ -1,22 +1,36 @@
+function dogImages() {
+  let image = document.createElement("img");
+  let liElement = document.createElement("li");
 
-const button = document.querySelector(".button");
-const ul = document.querySelector(".ul");
-const getImage = () => {
   fetch("https://dog.ceo/api/breeds/image/random")
-    .then((res) => res.json())
+    .then((response) => response.json())
     .then((data) => {
-      const liNew = document.createElement("li");
-      liNew.innerHTML = `<div id='container'>
-            <div class='container'>
-            <div class='img_div'><img src='${data.img}'/><div>
-            
-            </div>
-            </div>`;
-      ul.appendChild(liNew);
+      listElement.appendChild(liElement);
+      liElement.appendChild(image);
+
+      liElement.classList.add("no-bullet");
+      image.classList.add("img");
+
+      image.src = data.message;
     })
-        .catch((err) => console.log(err));
+    .catch((error) => {
+      errorDiv.innerHTML = "<p>Sorry... Not found</p>";
+      contentDiv.innerHTML = "";
+      contentDiv.appendChild(errorDiv);
+    });
+}
 
-button.addEventListener("click", getImage);
+let buttonEl = document.createElement("button");
+let contentDiv = document.querySelector("#content");
+let listElement = document.createElement("ul");
+let errorDiv = document.createElement("div");
 
-/*
-const button=document.queryElement
+buttonEl.innerText = "Dog";
+
+listElement.classList.add("list-element");
+errorDiv.classList.add("error-text");
+
+contentDiv.appendChild(buttonEl);
+contentDiv.appendChild(listElement);
+
+buttonEl.addEventListener("click", dogImages);
